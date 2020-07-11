4e6651fac2e0f473c61a55da2fb0f90908c6d50f
Leo Wang <leo.wang142001@gmail.com>
2020-07-11 13:40:17 -0400

    moved all image code to pieces.py

--- main.py
+++ main.py
@@ -28,95 +28,51 @@ def setup():
 
     # pawns
     for i in range(8):
-        image = pieces.subsurface((1750, 10, 332, 340))
-        image = pygame.transform.scale(image, (96, 96)).convert_alpha()
-        piece_list.append(Piece("Pawn", "White", image, chr(ord('a') + i) + '2'))
+        piece_list.append(Piece("Pawn", "White", chr(ord('a') + i) + '2'))
         # screen.blit(w_Pawn, (16+96*i, 16+96*6))
 
     for i in range(8):
-        image = pieces.subsurface((1750, 350, 332, 340))
-        image = pygame.transform.scale(image, (96, 96)).convert_alpha()
-        piece_list.append(Piece("Pawn", "Black", image, chr(ord('a') + i) + '7'))
+        piece_list.append(Piece("Pawn", "Black", chr(ord('a') + i) + '7'))
 
     # bishops
-    image = pieces.subsurface((703, 0, 340, 340))
-    image = pygame.transform.scale(image, (96, 96)).convert_alpha()
-    piece_list.append(Piece("Bishop", "White", image, 'c1'))
+    piece_list.append(Piece("Bishop", "White", 'c1'))
+    piece_list.append(Piece("Bishop", "White", 'f1'))
 
-    image = pieces.subsurface((703, 0, 340, 340))
-    image = pygame.transform.scale(image, (96, 96)).convert_alpha()
-    piece_list.append(Piece("Bishop", "White", image, 'f1'))
-
-    image = pieces.subsurface((703, 350, 340, 340))
-    image = pygame.transform.scale(image, (96, 96)).convert_alpha()
-    piece_list.append(Piece("Bishop", "Black", image, 'c8'))
-
-    image = pieces.subsurface((703, 350, 340, 340))
-    image = pygame.transform.scale(image, (96, 96)).convert_alpha()
-    piece_list.append(Piece("Bishop", "Black", image, 'f8'))
+    piece_list.append(Piece("Bishop", "Black", 'c8'))
+    piece_list.append(Piece("Bishop", "Black", 'f8'))
 
     # knights
-    image = pieces.subsurface((1050, 0, 340, 340))
-    image = pygame.transform.scale(image, (96, 96)).convert_alpha()
-    piece_list.append(Piece("Knight", "White", image, 'b1'))
+    piece_list.append(Piece("Knight", "White", 'b1'))
+    piece_list.append(Piece("Knight", "White", 'g1'))
 
-    image = pieces.subsurface((1050, 0, 340, 340))
-    image = pygame.transform.scale(image, (96, 96)).convert_alpha()
-    piece_list.append(Piece("Knight", "White", image, 'g1'))
-
-    image = pieces.subsurface((1050, 350, 340, 340))
-    image = pygame.transform.scale(image, (96, 96)).convert_alpha()
-    piece_list.append(Piece("Knight", "Black", image, 'b8'))
-
-    image = pieces.subsurface((1050, 350, 340, 340))
-    image = pygame.transform.scale(image, (96, 96)).convert_alpha()
-    piece_list.append(Piece("Knight", "Black", image, 'g8'))
+    piece_list.append(Piece("Knight", "Black", 'b8'))
+    piece_list.append(Piece("Knight", "Black", 'g8'))
 
     # rooks
-    image = pieces.subsurface((1398, 0, 340, 340))
-    image = pygame.transform.scale(image, (96, 96)).convert_alpha()
-    piece_list.append(Piece("Rook", "White", image, 'a1'))
+    piece_list.append(Piece("Rook", "White", 'a1'))
+    piece_list.append(Piece("Rook", "White", 'h1'))
 
-    image = pieces.subsurface((1398, 0, 340, 340))
-    image = pygame.transform.scale(image, (96, 96)).convert_alpha()
-    piece_list.append(Piece("Rook", "White", image, 'h1'))
-
-    image = pieces.subsurface((1398, 350, 340, 340))
-    image = pygame.transform.scale(image, (96, 96)).convert_alpha()
-    piece_list.append(Piece("Rook", "Black", image, 'a8'))
-
-    image = pieces.subsurface((1398, 350, 340, 340))
-    image = pygame.transform.scale(image, (96, 96)).convert_alpha()
-    piece_list.append(Piece("Rook", "Black", image, 'h8'))
+    piece_list.append(Piece("Rook", "Black", 'a8'))
+    piece_list.append(Piece("Rook", "Black", 'h8'))
 
     # queens and kings
-    image = pieces.subsurface((355, 0, 340, 340))
-    image = pygame.transform.scale(image, (96, 96)).convert_alpha()
-    piece_list.append(Piece("Queen", "White", image, 'd1'))
+    piece_list.append(Piece("Queen", "White", 'd1'))
+    piece_list.append(Piece("Queen", "Black", 'd8'))
 
-    image = pieces.subsurface((355, 350, 340, 340))
-    image = pygame.transform.scale(image, (96, 96)).convert_alpha()
-    piece_list.append(Piece("Queen", "Black", image, 'd8'))
-
-    image = pieces.subsurface((5, 0, 340, 340))
-    image = pygame.transform.scale(image, (96, 96)).convert_alpha()
-    piece_list.append(Piece("King", "Black", image, 'e1'))
-
-    image = pieces.subsurface((5, 350, 340, 340))
-    image = pygame.transform.scale(image, (96, 96)).convert_alpha()
-    piece_list.append(Piece("King", "Black", image, 'e8'))
+    piece_list.append(Piece("King", "White", 'e1'))
+    piece_list.append(Piece("King", "Black", 'e8'))
 
     return piece_list
 
 
 def drawWindow(window, piece_list):
     global board, pieces
+
     # draw the board
     board = pygame.transform.scale(board, (WIDTH, HEIGHT))
     screen.blit(board, (0,0))
 
     # draw pieces
-
     for piece in piece_list:
         piece.draw(screen)
 
--- pieces.py
+++ pieces.py
@@ -3,18 +3,70 @@
 import pygame
 pygame.init()
 
+pieces = pygame.image.load("Pieces.png") # spritesheet of all pieces
+
 # square names matched to their cords
 squares = {}
 for i in range(1, 9):
     for j, file in enumerate(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']):
         squares[file + str(i)] = (16 + 96*j, 800 - 16 - 96*i)
 
+
 class Piece(object):
 
-    def __init__(self, type, colour, image, position):
+    def __init__(self, type, colour, position):
         self.type = type # 'Pawn' 'Bishop' 'Rook' 'Knight' 'Queen' 'King'
         self.colour = colour
-        self.image = image
+        if colour == 'White':
+            if type == 'Pawn':
+                image = pieces.subsurface((1750, 10, 332, 340))
+                image = pygame.transform.scale(image, (96, 96)).convert_alpha()
+                self.image = image
+            elif type == 'Bishop':
+                image = pieces.subsurface((703, 0, 340, 340))
+                image = pygame.transform.scale(image, (96, 96)).convert_alpha()
+                self.image = image
+            elif type == 'Knight':
+                image = pieces.subsurface((1050, 0, 340, 340))
+                image = pygame.transform.scale(image, (96, 96)).convert_alpha()
+                self.image = image
+            elif type == 'Rook':
+                image = pieces.subsurface((1400, 0, 340, 340))
+                image = pygame.transform.scale(image, (96, 96)).convert_alpha()
+                self.image = image
+            elif type == 'Queen':
+                image = pieces.subsurface((355, 0, 340, 340))
+                image = pygame.transform.scale(image, (96, 96)).convert_alpha()
+                self.image = image
+            elif type == 'King':
+                image = pieces.subsurface((5, 0, 340, 340))
+                image = pygame.transform.scale(image, (96, 96)).convert_alpha()
+                self.image = image
+        elif colour == 'Black':
+            if type == 'Pawn':
+                image = pieces.subsurface((1750, 350, 332, 340))
+                image = pygame.transform.scale(image, (96, 96)).convert_alpha()
+                self.image = image
+            elif type == 'Bishop':
+                image = pieces.subsurface((703, 350, 340, 340))
+                image = pygame.transform.scale(image, (96, 96)).convert_alpha()
+                self.image = image
+            elif type == 'Knight':
+                image = pieces.subsurface((1050, 350, 340, 340))
+                image = pygame.transform.scale(image, (96, 96)).convert_alpha()
+                self.image = image
+            elif type == 'Rook':
+                image = pieces.subsurface((1400, 350, 340, 340))
+                image = pygame.transform.scale(image, (96, 96)).convert_alpha()
+                self.image = image
+            elif type == 'Queen':
+                image = pieces.subsurface((355, 350, 340, 340))
+                image = pygame.transform.scale(image, (96, 96)).convert_alpha()
+                self.image = image
+            elif type == 'King':
+                image = pieces.subsurface((5, 350, 340, 340))
+                image = pygame.transform.scale(image, (96, 96)).convert_alpha()
+                self.image = image
         self.position = position
 
 
